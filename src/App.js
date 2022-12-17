c87ca1044464a345e646d6bcd130c2e997bdb8d4
Jessica Reche <jessikareix20@gmail.com>
2022-12-17 20:57:59 +0000

    "Cambios hechos"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./styles.css";
+import ListOfGifs from "./components/ListOfGifs";
 
 export default function App() {
-  return (
-    <div className="App">`
-      <section className="App-content">
-        gifs
-      </section>
 
+
+  return (
+    <div className="App">
+      <section className="App-content">
+        <ListOfGifs keyword="witch" />
+      </section>
     </div>
   );
 }
